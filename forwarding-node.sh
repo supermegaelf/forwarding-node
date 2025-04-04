24189d740ff8eefae8918b37634caf1888047a1e
supermegaelf <81654098+supermegaelf@users.noreply.github.com>
2025-04-04 22:44:51 +0300

    Update forwarding-node.sh

--- forwarding-node.sh
+++ forwarding-node.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 read -p "Main public IP: " main_public_ip
+read -p "Enter port number (default is 10000): " port
+port=${port:-10000}
 
 apt install ufw -y
 
@@ -9,13 +11,13 @@ echo 'net/ipv4/ip_forward=1' >> /etc/ufw/sysctl.conf
 cat >> /etc/ufw/before.rules << EOF
 *nat
 :PREROUTING ACCEPT [0:0]
--A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 10000
+-A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port $port
 COMMIT
 EOF
 
 ufw allow 443/tcp comment "HTTPS (Reality)" && ufw allow 22/tcp comment "SSH"
 
-ufw allow 10000/tcp comment 'HTTPS (Reality)'
+ufw allow $port/tcp comment 'HTTPS (Reality)'
 
 ufw allow from "$main_public_ip" to any port 62050 proto tcp comment 'Marzmain'
 
